03b5e554af7dad7d78e030c131ae986f01fbe8f0
Elijah Wilcott <ejw773@gmail.com>
2021-12-06 22:42:31 -0500

    finishes main.js

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
+// done
+
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+// import Constants from 'expo-constants';
+import Main from './src/screens/Main';
+import { store } from './src/redux/store';
 import { Provider } from 'react-redux';
-
-
-
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <Main />
+      </SafeAreaProvider>
+    </Provider>
   );
 }
