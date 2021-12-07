2a6561e326855591ebbe2b3fe6fc60684c1f9323
Elijah Wilcott <ejw773@gmail.com>
2021-12-06 22:10:44 -0500

    adds some dependencies

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Constants from 'expo-constants';
+import Main from './screens/Main';
+import { store } from './src/redux/store';
+import { Provider } from 'react-redux';
+
+import { View, Text } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
