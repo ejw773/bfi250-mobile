6a742d5bde3947760696e69a5f75137e57732ce3
Elijah Wilcott <ejw773@gmail.com>
2021-12-06 22:12:13 -0500

    reverts to original

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
-import * as React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import Constants from 'expo-constants';
-import Main from './screens/Main';
-import { store } from './src/redux/store';
-import { Provider } from 'react-redux';
-
-import { View, Text } from 'react-native'
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
