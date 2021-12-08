b428b4d8ff335e210d588de87431f06c9a67399b
Elijah Wilcott <ejw773@gmail.com>
2021-12-07 23:32:28 -0500

    adds local storage

--- src/services/film-service.js
+++ src/services/film-service.js
@@ -1,10 +1,18 @@
 import axios from 'axios'
 import { API_URL } from '../api/apiUrl'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const filmService = async () => {
     try {
         const response = await axios.get(API_URL + 'ranks/all/withfilms/')
-        //localStorage.setItem('films', JSON.stringify(response.data))
+        const storeData = async (response) => {
+            try {
+                const jsonValue = JSON.stringify(response)
+                await AsyncStorage.setItem('films', jsonValue)
+            } catch (e) {
+                return e;
+            }
+        }
         return response.data
     } catch (e) {
         console.log(e)
